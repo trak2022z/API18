b474bc7746005caa6781cc59796cc5aa41210b03
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2023-04-01 19:33:12 +0000

    1

--- script.js
+++ script.js
@@ -1,71 +1,77 @@
-(function() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+      
+      // Function to fetch all posts
+      function fetchPosts() {
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((posts) => console.log(posts))
+          .catch((error) => console.error(error));
+      }
+      
+      // Function to fetch a single post
+      function fetchPost() {
+        const postId = document.getElementById('postIdInput').value;
+        fetch(`${apiUrl}/${postId}`)
+          .then((response) => response.json())
+          .then((post) => console.log(post))
+          .catch((error) => console.error(error));
+      }
+      
+      // Function to create a new post
+      function createPost() {
+        const title = document.getElementById('titleInput').value;
+        const body = document.getElementById('bodyInput').value;
+        const userId = document.getElementById('userIdInput').value;
+        fetch(apiUrl, {
+          method: 'POST',
+          body: JSON.stringify({
+            title: title,
+            body: body,
+            userId: userId
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then((response) => response.json())
+          .then((newPost) => console.log(newPost))
+          .catch((error) => console.error(error));
+      }
 
-  window.addEventListener('load', init);
 
-  function init() {
-    qsa('section button').addEventListener('click', makeRequest);
-  }
-
-  function makeRequest() {
-    let c = id('category').value;
-    let q = id('question').value;
-    let a = id('answer').value;
-
-    fetch('https://courses.cs.washington.edu/courses/cse154/webservices/trivia/trivia.php', {
-      method: 'POST',
-      body: new URLSearchParams({
-        category: c,
-        question: q,
-        answer: a
-      })
-    })
-      .then(statusCheck)
-      .then(res => res.text())
-      .then(message => {
-        let container = gen('section');
-
-        let p1 = gen('p');
-        p1.innerHTML = 'category: ' + c;
-        container.appendChild(p1);
-
-        let p2 = gen('p');
-        p2.innerHTML = 'question: ' + q;
-        container.appendChild(p2);
-
-        let p3 = gen('p');
-        p3.innerHTML = 'answer: ' + a;
-        container.appendChild(p3);
-
-        let p4 = gen('p');
-        p4.innerHTML = 'API Response: ' + message;
-        container.appendChild(p4);
-
-        id('trivia').appendChild(container);
-      })
-      .catch(error => console.error(error));
-  }
-
-  function statusCheck(response) {
-    if (!response.ok) {
-      throw new Error(response.statusText);
+    function updatePost() {
+          const postId = document.getElementById('updatePostIdInput').value;
+          const title = document.getElementById('updateTitleInput').value;
+          const body = document.getElementById('updateBodyInput').value;
+  fetch(`${apiUrl}/${postId}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title: title,
+      body: body
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
-    return response;
-  }
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+}
 
-  function qsa(query) {
-    return document.querySelectorAll(query);
-  }
 
-  function qs(query) {
-    return document.querySelector(query);
-  }
-
-  function id(id) {
-    return document.getElementById(id);
-  }
-
-  function gen(el) {
-    return document.createElement(el);
-  }
-
-})();
+function deletePost() {
+  const postId = document.getElementById('updatePostIdInput').value;
+  const title = document.getElementById('updateTitleInput').value;
+  const body = document.getElementById('updateBodyInput').value;
+  fetch(`${apiUrl}/${postId}`, {
+    method: 'DELETE'
+  })
+  .then(response => {
+    if (response.status === 200) {
+      console.log(`Post ${postId} deleted successfully`);
+    } else {
+      console.error(`Error deleting post ${postId}`);
+    }
+  })
+  .catch(error => console.error(error));
+}
